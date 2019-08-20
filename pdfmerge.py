61d9183b6b36af539098ed75ea5732e7ee04bb21
Ted Trentler <ted@trentlerconsulting.com>
2019-08-20 08:53:27 -0400

    Add files via upload
    pdfmerge is a python3 script that uses the pyPDF4 library to merge all pdf files in a single directory into one pdf file.
    
    Use pip install pypdf4 to install the necessary library.

---
+++ pdfmerge.py
@@ -0,0 +1,28 @@
+#! uses python3 with pyPDF4
+# combinePdfs.py - Combines all the PDFs in the current working directory into 
+# a single PDF.
+
+import PyPDF4, os
+
+# Get all the PDF filenames.
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort()
+
+pdfWriter = PyPDF4.PdfFileWriter()
+
+# Loop through all the PDF files.
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
+# Loop through all the pages (sarting at zero) and add them.
+    for pageNum in range(0, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+# Save the resulting PDF to a file.
+pdfOutput = open('combined_pdfs.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
